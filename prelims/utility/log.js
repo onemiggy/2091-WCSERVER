48150b8afa66939b41fe5a4f86dc958d26009de5
onemiggy <123311817+onemiggy@users.noreply.github.com>
2023-01-23 11:43:29 +0800

    Add files via upload
    initial changes

---
+++ prelims/utility/log.js
@@ -0,0 +1,14 @@
+var log = {
+    info:function(info){
+        comsole.log("Info:");
+    },
+
+    warning:function(warning){
+        console.log('Warning:' + warning);
+},
+    warning:function(error){
+        console.log('Error: ' + warning);
+}
+
+};    
+
