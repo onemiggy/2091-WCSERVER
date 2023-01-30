18a0da7d781ab7982cf9105142f6684b49131962
onemiggy <123311817+onemiggy@users.noreply.github.com>
2023-01-30 12:41:49 +0800

    1 file modified

--- prelims/Prelim-Lab-Activity-3/Lab-Act-3-NodeJs-server.js
+++ prelims/Prelim-Lab-Activity-3/Lab-Act-3-NodeJs-server.js
@@ -4,7 +4,7 @@ var server = http.createServer(function (req, res) {
   if (req.url == '/') {
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write(
-      '<html><h1>This is Home Page.</h1><body><p>Juan Miguel T. Enriquez.</p></body></html>'
+      '<html><h1>Welcome to my Node.JS Application.</h1><body><p>Welcome Juan Miguel Enriquez. This is an activity about basics of node JS</p></body></html>'
     );
     res.end();
   } else if (req.url == '/student') {
