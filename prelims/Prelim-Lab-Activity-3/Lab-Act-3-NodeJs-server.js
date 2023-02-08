6c8572daea0ad0a08caf4d30e298860728909489
onemiggy <123311817+onemiggy@users.noreply.github.com>
2023-02-08 11:06:13 +0800

    1 file modified

--- prelims/Prelim-Lab-Activity-3/Lab-Act-3-NodeJs-server.js
+++ prelims/Prelim-Lab-Activity-3/Lab-Act-3-NodeJs-server.js
@@ -7,14 +7,21 @@ var server = http.createServer(function (req, res) {
       '<html><h1>Welcome to my Node.JS Application.</h1><body><p>Welcome Juan Miguel Enriquez. This is an activity about basics of node JS</p></body></html>'
     );
     res.end();
-  } else if (req.url == '/student') {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write('<html><body><p>This is about page Page.</p></body></html>');
-    res.end();
   } else if (req.url == '/contact') {
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write(
+      '<html><body><p>This is about page Page.</p><p>Hello Miguel Enriquez, if you want additional details about this activity go to this site https://www.tutorialsteacher.com/nodejs/nodejs-tutorials</p></body></html>'
+    );
+    res.end();
+  } else if (req.url == '/about') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(
       '<html><body><p>This is The Student Page.</p><p>Hello Enriquez Juan Miguel, This activty will teach you on how to create a simple server and local moduels in Nodejs.</p></body></html>'
+    );
+  } else if (req.url == '/gallery') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(
+      '<html><body><p>This is Gallery.</p><p>Hello Enriquez Juan Miguel, This activty will teach you on how to create a simple server and local moduels in Nodejs.</p></body></html>'
     );
     res.end();
   } else res.end('Invalid Request!');
