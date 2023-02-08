b697e578f5003b91a2a57fcd1cbaddf127f05998
onemiggy <123311817+onemiggy@users.noreply.github.com>
2023-02-08 16:04:28 +0800

    2 files added

---
+++ prelims/Hands-On-Exam/app.js
@@ -0,0 +1,5 @@
+var greetings = require('./welcome.js');
+
+console.log(greetings.greet('Juan Miguel T. Enriquez'));
+console.log(greetings.congrats('Juan Miguel T. Enriquez'));
+console.log(greetings.farewell('Juan Miguel T. Enriquez'));
---
+++ prelims/Hands-On-Exam/welcome.js
@@ -0,0 +1,17 @@
+function greet(name) {
+  return 'Greetings, ' + name;
+}
+
+function congrats(name) {
+  return 'Congratualations, ' + name;
+}
+
+function farewell(name) {
+  return 'Farewell, ' + name;
+}
+
+module.exports = {
+  greet: greet,
+  congrats: congrats,
+  farewell: farewell,
+};
