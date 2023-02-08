a0e093634f61acffe971037822b2ca47191f0e13
onemiggy <123311817+onemiggy@users.noreply.github.com>
2023-02-08 10:56:20 +0800

    1 file modified

--- prelims/Prelim-Lab-Activity-3/Lab-Act-3-NodeJs-server.js
+++ prelims/Prelim-Lab-Activity-3/Lab-Act-3-NodeJs-server.js
@@ -9,7 +9,7 @@ var server = http.createServer(function (req, res) {
     res.end();
   } else if (req.url == '/student') {
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write('<html><body><p>This is The Student Page.</p></body></html>');
+    res.write('<html><body><p>This is about page Page.</p></body></html>');
     res.end();
   } else if (req.url == '/admin') {
     res.writeHead(200, { 'Content-Type': 'text/html' });
