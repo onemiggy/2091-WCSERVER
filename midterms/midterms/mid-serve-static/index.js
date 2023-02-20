fcb295149dccf6ee0fd8568319bd678cf7d213b8
onemiggy <123311817+onemiggy@users.noreply.github.com>
2023-02-20 10:04:38 +0800

    3 files modified

--- midterms/midterms/mid-serve-static/index.js
+++ midterms/midterms/mid-serve-static/index.js
@@ -7,11 +7,11 @@ app.use(express.static('public'));
 
 //crate the roue to serve a static index.html
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/' + 'index.html')
+  res.sendFile(__dirname + '/' + 'index.html');
 });
 
 //setting the listener to rnv port info
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`listening on port ${port}`)
+  console.log(`listening on port ${port}`);
 });
